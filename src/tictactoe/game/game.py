a9853bc6063031b996efde597519f30fc732e716
zlucasftw <ilucasmcpvp@gmail.com>
2026-08-13 01:37:03 -0300

    refactor(game): use lifecycle status for moves

--- src/tictactoe/game/game.py
+++ src/tictactoe/game/game.py
@@ -28,7 +28,7 @@ class TicTacToeGame:
 
     def play_move(self, row: int, column: int) -> bool:
         """Play a move for the current player when the move is legal."""
-        if self.board.is_game_over():
+        if self.status != GameStatus.IN_PROGRESS:
             return False
 
         move_was_played = self.board.make_move(row, column, self.current_player)
